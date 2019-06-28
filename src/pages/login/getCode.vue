d55d5231a56fdfebb7f09f60721f7ba98d9646eb
wwy <811585541@qq.com>
2019-06-28 14:08:45 +0800

    扫码

--- src/pages/login/getCode.vue
+++ src/pages/login/getCode.vue
@@ -29,7 +29,13 @@ export default {
             // alert(seeUid)
             // localStorage.setItem(recommadd,this.GetQueryString('recommadd'))
             // localStorage.setItem(seeUid,this.GetQueryString('uid'))
-            var redirect_uri = encodeURI("http://www.shuimujiajia.net/app/index.html?recommadd="+recommadd+"_"+seeUid);
+             var redirect_uri ;
+            if(recommadd){
+                 redirect_uri = encodeURI("http://www.shuimujiajia.net/app/index.html?recommadd="+recommadd+"_"+seeUid);
+            }else{
+                 redirect_uri = encodeURI("http://www.shuimujiajia.net/app/index.html?");
+            }
+            // var redirect_uri = encodeURI("http://www.shuimujiajia.net/app/index.html?recommadd="+recommadd+"_"+seeUid);
             var url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + redirect_uri + "&response_type=code&scope=snsapi_userinfo&state=STATE&connect_redirect=1#wechat_redirect"
             window.location.href = url;
         },
